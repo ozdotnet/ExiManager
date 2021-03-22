762ac10d204abc98314909bf1b13edd93836a7a0
Ozkan B <ozkanbeserek@hotmail.com>
2021-03-22 21:22:17 +0100

    DbContextFactory class

--- ExiManager.EntityFramework/ExiManagerDbContext.cs
+++ ExiManager.EntityFramework/ExiManagerDbContext.cs
@@ -1,25 +1,17 @@
 ﻿using ExiManager.Shared.Model;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ExiManager.EntityFramework
 {
-    public class ExiManagerDbContext : DbContext
+    public class ExiManagerDbContext: DbContext
     {
         public DbSet<Folder> Folders { get; set; }
         public DbSet<File> Files { get; set; }
         public DbSet<ExiInfo> ExiInfos { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        public ExiManagerDbContext(DbContextOptions options) : base(options)
         {
-            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False;Database=ExiManagerDB;Trusted_Connection=True;");
 
-
-            base.OnConfiguring(optionsBuilder);
         }
     }
 }
---
+++ ExiManager.EntityFramework/ExiManagerDbContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace ExiManager.EntityFramework
+{
+    public class ExiManagerDbContextFactory : IDesignTimeDbContextFactory<ExiManagerDbContext>
+    {
+        public ExiManagerDbContext CreateDbContext(string[] args)
+        {
+            var options = new DbContextOptionsBuilder<ExiManagerDbContext>();
+            options.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=ExiManagerDB;Trusted_Connection=True;");
+
+            return new ExiManagerDbContext(options.Options);
+        }
+    }
+}
