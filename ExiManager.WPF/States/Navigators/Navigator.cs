2a318ab5aa4d59c728dd45044bb8012f3a20a26d
Ozkan B <ozkanbeserek@hotmail.com>
2021-03-24 20:21:14 +0100

    Generic ObservableObject implementing INotifyPropertyChanged

--- ExiManager.WPF/States/Navigators/Navigator.cs
+++ ExiManager.WPF/States/Navigators/Navigator.cs
@@ -1,12 +1,23 @@
 ﻿using ExiManager.WPF.Commands;
+using ExiManager.WPF.Models;
 using ExiManager.WPF.ViewModels;
 using System.Windows.Input;
 
 namespace ExiManager.WPF.States.Navigators
 {
-    public class Navigator : INavigator
+    public class Navigator : ObservableObject, INavigator
     {
-        public ViewModelBase CurrentViewModel { get; set; }
+        private ViewModelBase _currentViewModel;
+        public ViewModelBase CurrentViewModel
+        {
+            get { return _currentViewModel; }
+            set 
+            { 
+                _currentViewModel = value;
+                OnPropertyChanged(nameof(CurrentViewModel));
+            }
+        }
+
         public ICommand UpdateCurrentViewModelCommand => new UpdateCurrentViewModelCommand(this);
     }
 }
